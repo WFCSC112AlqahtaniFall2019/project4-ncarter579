9c16d1c9bea5591d3b7a530956e87115396e3c45
Nathan Carter <cartnl18@wfu.edu>
2019-10-03 16:44:50 -0400

    Base code for the game is finished. Still needs touching up

--- TTTGame.cpp
+++ TTTGame.cpp
@@ -25,37 +25,85 @@ int main() {
     game.printBoard();
 
     if(pCount == 2){
-        for(int i = 0; i < 9; i ++){
+        for(int i = 0; i < 100; i ++){
             cout << "player " << game.getCurrentPlayerNumber() <<" enter your row (1-3)" << endl;
             cin >> row;
             cout << "player " << game.getCurrentPlayerNumber() << " enter your column (1-3)" << endl;
             cin >> col;
-            game.placeMark(row - 1, col - 1);
-            game.printBoard();
+            if( game.placeMark(row - 1, col - 1) == true) {
+                game.placeMark(row - 1, col - 1);
+                game.printBoard();
 
-            if(game.checkForWin() == true){
-                cout << "Player " << game.getCurrentPlayerNumber() << " Wins!!!" << endl;
-                return 0;
+                if (game.checkForWin() == true) {
+                    cout << "Player " << game.getCurrentPlayerNumber() << " Wins!!!" << endl;
+                    return 0;
+                } else {
+                    game.changePlayer();
+                }
+                if (game.isBoardFull() == true) {
+                    cout << "Game over! No One Wins!" << endl;
+                    return 0;
+                }
             }
             else{
-                game.changePlayer();
+                cout << "not a valid entry, please enter a valid row and column combination" << endl;
             }
         }
-        if(game.isBoardFull() == true){
-            cout << "Game over! No One Wins!" << endl;
-        }
+
     }
     else{
         goesFirst = rand() % 2 + 0;
         if (goesFirst == 1){
             game.changePlayer();
         }
-        for(int i = 0; i < 9; i ++){
+        cout << "It's player " << game.getCurrentPlayerNumber() << "'s turn!" << endl;
+        for(int i = 0; i < 500; i ++){
             if(game.getCurrentPlayerNumber() == 1){    //if the human is playing
+                cout << "player " << game.getCurrentPlayerNumber() <<" enter your row (1-3)" << endl;
+                cin >> row;
+                cout << "player " << game.getCurrentPlayerNumber() << " enter your column (1-3)" << endl;
+                cin >> col;
+                if( game.placeMark(row - 1, col - 1) == true) {
+                    game.placeMark(row - 1, col - 1);
+                    game.printBoard();
 
+                    if (game.checkForWin() == true) {
+                        cout << "Player " << game.getCurrentPlayerNumber() << " Wins!!!" << endl;
+                        return 0;
+                    } else {
+                        game.changePlayer();
+                    }
+                    if (game.isBoardFull() == true) {
+                        cout << "Game over! No One Wins!" << endl;
+                        return 0;
+                    }
+                }
+                else{
+                    cout << "not a valid entry, please enter a valid row and column combination" << endl;
+                }
             }
             else if (game.getCurrentPlayerNumber() == 2){   //if the robot is playing
+                row = rand() % 3 + 0;
+                col = rand() % 3 + 0;
+                if( game.placeMark(row, col) == true){
+                    game.placeMark(row, col);
+                    game.printBoard();
 
+                    if(game.checkForWin() == true){
+                        cout << "Player " << game.getCurrentPlayerNumber() << " Wins!!!" << endl;
+                        return 0;
+                    }
+                    else{
+                        game.changePlayer();
+                    }
+                    if(game.isBoardFull() == true){
+                        cout << "Game over! No One Wins!" << endl;
+                        return 0;
+                    }
+                }
+                else{
+                    cout << "not a valid entry, please enter a valid row and column combination" << endl;
+                }
             }
         }
     }
